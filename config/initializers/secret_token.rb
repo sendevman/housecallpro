5013c17a542a609185435d46d6d47627bc85e8fe
sendevman <devman.sd@gmail.com>
2017-01-29 07:18:49 +0000

    base rails app with foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HousecallConsumerWeb::Application.config.secret_token = '17b433148c73cab664ad508253c6c76757f8eaca9a72e56860e9a48aeb6931f6cd14e48c7191d94bbafb3f4e49125e51166e391819cae39a2c81a6c7ae05efb3'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+HousecallConsumerWeb::Application.routes.draw do
+
+  root :to => 'home#index'
+
+end
